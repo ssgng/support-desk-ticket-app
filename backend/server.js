47d4badb1eb340407e079628fd063a1a0aceb749
Selim Sami <Selim.Genc@hhu.de>
2023-06-22 16:42:48 +0200

    created backend section and connected to the mongoDB. Trying to create the file structure of the project

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log("server..")
+const path = require('path')
+const express = require('express')
+require('colors') 
+require('dotenv').config()
+const {errorHandler} = require("./middleware/errorMiddleware")
+const PORT = process.env.PORT || 5001
+const connectDB = require("./config/db")
+
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get("/", (req,res)=>{
+    res.json({message: "Welcome to the Support Desk API"})
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
